d866905f0b35071987baa903f56dad50b9c2750f
Karl Krägelin <8436596+karkraeg@users.noreply.github.com>
2023-09-12 14:58:42 +0200

    Update Iconclass.php to use the new API

--- Iconclass.php
+++ Iconclass.php
@@ -1,15 +1,15 @@
 <?php
 /** ---------------------------------------------------------------------
- * app/lib/core/Plugins/InformationService/Iconclass.php :
+ * app/lib/Plugins/InformationService/Iconclass.php :
  * ----------------------------------------------------------------------
- * Iconclass InformationService by Karl Becker 2017
+ * Iconclass InformationService by Karl Krägelin 2017-2023
  * ----------------------------------------------------------------------
  * CollectiveAccess
  * Open-source collections management software
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2015 Whirl-i-Gig
+ * Copyright 2015-2018 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -34,14 +34,26 @@
 
   /**
     *
-    */ 
-    
-    
-require_once(__CA_LIB_DIR__."/core/Plugins/IWLPlugInformationService.php");
-require_once(__CA_LIB_DIR__."/core/Plugins/InformationService/BaseInformationServicePlugin.php");
+    */
+
+
+require_once(__CA_LIB_DIR__."/Plugins/IWLPlugInformationService.php");
+require_once(__CA_LIB_DIR__."/Plugins/InformationService/BaseInformationServicePlugin.php");
 
 global $g_information_service_settings_Iconclass;
-$g_information_service_settings_Iconclass = array();
+$g_information_service_settings_Iconclass = [
+    'numberOfresults' => array(
+        'formatType' => FT_TEXT,
+        'displayType' => DT_FIELD,
+        'default' => 6,
+        'width' => 10,
+        'height' => 1,
+        'label' => _t('Number of results'),
+        'validForRootOnly' => 1,
+        'description' => _t('Enter an integer value for my setting.'),
+    ),
+
+];
 
 class WLPlugInformationServiceIconclass Extends BaseInformationServicePlugin Implements IWLPlugInformationService {
 	# ------------------------------------------------
@@ -56,11 +68,11 @@ class WLPlugInformationServiceIconclass Extends BaseInformationServicePlugin Imp
 		WLPlugInformationServiceIconclass::$s_settings = $g_information_service_settings_Iconclass;
 		parent::__construct();
 		$this->info['NAME'] = 'Iconclass';
-		
+
 		$this->description = _t('Provides access to Iconclass service');
 	}
 	# ------------------------------------------------
-	/** 
+	/**
 	 * Get all settings settings defined by this plugin as an array
 	 *
 	 * @return array
@@ -71,7 +83,7 @@ class WLPlugInformationServiceIconclass Extends BaseInformationServicePlugin Imp
 	# ------------------------------------------------
 	# Data
 	# ------------------------------------------------
-	/** 
+	/**
 	 * Perform lookup on Iconclass-based data service
 	 *
 	 * @param array $pa_settings Plugin settings values
@@ -80,31 +92,44 @@ class WLPlugInformationServiceIconclass Extends BaseInformationServicePlugin Imp
 	 * @return array
 	 */
 	public function lookup($pa_settings, $ps_search, $pa_options=null) {
-
+        global $g_ui_locale;
+		if ($vs_locale = ($g_ui_locale) ? $g_ui_locale : __CA_DEFAULT_LOCALE__) {
+			$vs_lang = strtolower(array_shift(explode("_", $vs_locale)));
+		} else {
+			$vs_lang = 'en';
+		}
+        $va_number_of_results = $pa_settings['numberOfresults'];
+        // query the Iconclass API
 		$vs_content = caQueryExternalWebservice(
-            $vs_url = 'http://iconclass.org/rkd/9/?q='.urlencode($ps_search).'&q_s=1&fmt=json'
+            $vs_url = 'https://iconclass.org/api/search?q='.urlencode($ps_search).'&lang='.$vs_lang.'&size=999&page=1&sort=rank&keys=0'
 		);
-
+        // decode the JSON response
 		$va_content = @json_decode($vs_content, true);
-		if(!isset($va_content['records']) || !is_array($va_content['records'])) { return array(); }
+		// check if $va_content has a key called "records" and if the value of that key is an array. If either of these conditions is not true, return an empty array.
+        if(!isset($va_content['result']) || !is_array($va_content['result'])) { return []; }
 
-		// the top two levels are 'diagnostic' and 'records'
-		$va_results = $va_content['records'];
-		$va_return = array();
+		// extract the value of the "result" key from an associative array $va_content and assigns it to a new variable $va_results.
+		$va_results = array_slice($va_content['result'], 0, $va_number_of_results);
+		// initialize a new empty array $va_return.
+        $va_return = [];
 
 		foreach($va_results as $va_result) {
+			// for each id in $va_results call 'https://iconclass.org/'.$va_result.'.json'
+            // and assign the result to a new variable $vs_content.
+            $id_content = caQueryExternalWebservice($vs_url = 'https://iconclass.org/'.$va_result.'.json');
+            $id_content_decoded = @json_decode($id_content, true);
 
-			$va_return['results'][] = array(
-				'label' => $va_result['txt']['de'],
-				'url' => 'http://iconclass.org/'.$va_result['n'],
-				'idno' => $va_result['n'],
-			);
+            $va_return['results'][] = [
+				'label' => $id_content_decoded['txt']['de'],
+				'url' => 'http://iconclass.org/de/'.$va_result,
+				'idno' => $va_result,
+            ];
 		}
 
 		return $va_return;
 	}
 	# ------------------------------------------------
-	/** 
+	/**
 	 * Fetch details about a specific item from a Iconclass-based data service for "more info" panel
 	 *
 	 * @param array $pa_settings Plugin settings values
@@ -112,9 +137,9 @@ class WLPlugInformationServiceIconclass Extends BaseInformationServicePlugin Imp
 	 * @return array An array of data from the data server defining the item.
 	 */
 	public function getExtendedInformation($pa_settings, $ps_url) {
-		$vs_display = "<p><a href='$ps_url' target='_blank'>$ps_url</a></p>";
+		$vs_display = "<p><a href='{$ps_url}' target='_blank'>{$ps_url}</a></p>";
 
-		return array('display' => $vs_display);
+		return ['display' => $vs_display];
 	}
 	# ------------------------------------------------
 }
